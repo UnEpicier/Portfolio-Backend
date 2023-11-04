ca08e9fcc15762bbbc836ee0a8be288da2f9a0df
UnEpicier <alexisvasseur96@gmail.com>
2023-11-04 11:05:25 +0100

    Create skill route

--- src/skill/skill.controllers.ts
+++ src/skill/skill.controllers.ts
@@ -11,7 +11,7 @@ import { Sequelize } from 'sequelize';
 import { defineModelSkill } from '../models/skill';
 // ---------------------------------------------------------------------------------------------------------------------
 
-export async function getDbSkill() {
+export async function getDbSkills() {
 	const dbConn = new Sequelize({
 		dialect: 'sqlite',
 		storage: `${process.cwd()}/databases/general.db`,
@@ -32,5 +32,31 @@ export async function getDbSkill() {
 		success: true,
 		message: 'Successfully getted skills',
 		skills: dbSkills,
+	};
+}
+
+export async function createDbSkill(name: string) {
+	const dbConn = new Sequelize({
+		dialect: 'sqlite',
+		storage: `${process.cwd()}/databases/general.db`,
+		logging: false,
+	});
+
+	const skillModel = defineModelSkill(dbConn);
+
+	try {
+		skillModel.create({
+			name: name,
+		});
+	} catch {
+		return {
+			success: false,
+			message: `Can't create ${name} skill.`,
+		};
+	}
+
+	return {
+		success: true,
+		message: `Successfully created ${name} skill.`,
 	};
 }
--- src/skill/skill.services.ts
+++ src/skill/skill.services.ts
@@ -7,7 +7,7 @@ import { Request, Response } from 'express';
 // ---------------------------------------------------------------------------------------------------------------------
 
 // --------------------------------------------------- Controllers -----------------------------------------------------
-import { getDbSkill } from './skill.controllers';
+import { createDbSkill, getDbSkills } from './skill.controllers';
 // ---------------------------------------------------------------------------------------------------------------------
 
 // ------------------------------------------------------ Utils --------------------------------------------------------
@@ -15,12 +15,40 @@ import { verifyToken } from '../utils/auth';
 // ---------------------------------------------------------------------------------------------------------------------
 
 export async function getSkills(req: Request, res: Response) {
-	const dbSkills = await getDbSkill();
+	const dbSkills = await getDbSkills();
 
 	return res.status(dbSkills.success ? 200 : 500).json(dbSkills);
 }
 
-export async function createSkill(req: Request, res: Response) {}
+export async function createSkill(req: Request, res: Response) {
+	const token = req.headers.authorization;
+	const { name } = req.body;
+
+	if (!token) {
+		return res.status(401).json({
+			success: false,
+			message: 'A valid token is required to create a skill.',
+		});
+	}
+
+	if (!(await verifyToken(token))) {
+		return res.status(404).json({
+			success: false,
+			message: 'Provided token not found.',
+		});
+	}
+
+	if (!name) {
+		return res.status(400).json({
+			success: false,
+			message: 'Name field is missing in request body.',
+		});
+	}
+
+	const skillCreated = await createDbSkill(name);
+
+	return res.status(skillCreated.success ? 200 : 500).json(skillCreated);
+}
 
 export async function updateSkill(req: Request, res: Response) {}
 
