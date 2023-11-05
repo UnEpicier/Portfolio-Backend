8bfbcf8f080a4002530f8dddbdc6c1dab5d1d7e5
UnEpicier <alexisvasseur96@gmail.com>
2023-11-05 16:50:25 +0100

    About routes

--- src/models/about.ts
+++ src/models/about.ts
@@ -3,11 +3,35 @@
 // ---------------------------------------------------------------------------------------------------------------------
 
 // ---------------------------------------------------- Sequelize ------------------------------------------------------
-import { Sequelize, DataTypes } from 'sequelize';
+import {
+	Sequelize,
+	DataTypes,
+	Model,
+	InferAttributes,
+	InferCreationAttributes,
+	CreationOptional,
+} from 'sequelize';
 // ---------------------------------------------------------------------------------------------------------------------
 
+// Create/Update the about table
+// const dbConn = new Sequelize({
+// 	dialect: 'sqlite',
+// 	storage: `${process.cwd()}/databases/general.db`,
+// 	logging: false,
+// });
+// const model = defineModelAbout(dbConn);
+// model.sync({ alter: true });
+
+class About extends Model<
+	InferAttributes<About>,
+	InferCreationAttributes<About>
+> {
+	declare image: string;
+	declare description: string;
+}
+
 export function defineModelAbout(db: Sequelize) {
-	return db.define('about', {
+	return db.define<About>('about', {
 		image: {
 			type: DataTypes.TEXT,
 			defaultValue: 'about/default.png',
