ff9e973972e754dca3c80501ab1d57e7498da0de
UnEpicier <alexisvasseur96@gmail.com>
2023-11-13 15:15:21 +0100

    Fix undefined port number

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,8 @@ WORKDIR /src
 COPY package*.json ./
 EXPOSE 3000
 
-FROM base as production
 ENV NODE_ENV=production
 RUN npm ci
 COPY . .
 RUN npm run build
 CMD ["npm", "start"]
-
-FROM base as dev
-ENV NODE_ENV=development
-RUN npm install
-COPY . .
-CMD ["npm", "run", "dev"]
--- src/index.ts
+++ src/index.ts
@@ -53,6 +53,6 @@ app.use('/', about);
 app.use('/', experience);
 app.use('/', formation);
 
-app.listen(process.env.PORT ?? 3000, () => {
-	console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(3000, () => {
+	console.log(`Server is running on port ${3000}`);
 });
