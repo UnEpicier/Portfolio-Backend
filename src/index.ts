27070bdff6103beb84409158b611bde1e746825d
UnEpicier <alexisvasseur96@gmail.com>
2023-12-04 11:02:54 +0100

    fix: increase to 50mb body size limit

--- src/index.ts
+++ src/index.ts
@@ -40,10 +40,22 @@ app.use(cors());
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 app.use(
 	bodyParser.urlencoded({
+		limit: '50mb',
 		extended: true,
+		parameterLimit: 50000,
 	}),
 );
-app.use(bodyParser.json());
+app.use(
+	bodyParser.json({
+		limit: '50mb',
+	}),
+);
+
+app.use(
+	bodyParser.text({
+		limit: '50mb',
+	}),
+);
 
 app.use('/', auth);
 app.use('/', skill);
